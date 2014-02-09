ae0d7c90deebc17f185c9348eb5301cc33f69319
Olle <o.brickarp@gmail.com>
2014-02-09 15:49:32 +0100

    Added support for settings and multiple rsb's with individual settings

--- README.md
+++ README.md
@@ -1,33 +1,61 @@
 # Radial Share Buttons (rsb)
-I made this to learn more about Javascript and to actually make something that works. Still very early in development but I've had fun this far so it might go places.
+I made this to learn more about Javascript and to actually make something that works. Still early in development but I've had fun this far so it might go places.
+
 
 ##What it does
-Radial Share Buttons, or rsb for short, is a simple little share plugin. It used jquery and css3 to show and animate five (as of right now) preset share options; Facebook, Twitter, Pinterest, Google+ and E-mail. It takes up a small space (50x50px normal and 200x125px expanded) and is easily positioned. The script can handle multiple buttons on a single page (see demo.html) but are right now limited to the same settings.
+Radial Share Buttons, or rsb for short, is a simple share button. It used jquery and css3 to show and animate five (as of right now) preset share options; Facebook, Twitter, Pinterest, Google+ and E-mail. It takes up a small space (50x50px normal and 200x125px expanded) and is easily positioned. The script can handle multiple buttons on a single page (see demo.html). You can configure rsb so that multiple buttons use the same settings or use their own. These can be mixed so that two buttons use the same settings and a third use it's own settings.
 
-##Settings
-As of right now all settings are setup in the rsb.js file. These will move to a more convenient place as soon as I learn how to do that... You can change, per button, what should be shared and what to use as message/description. Twitter also has an option to show your twitter handle in the post (via). Below is a list of available variables (all values are strings):
-_Facebook variables_
-* facebookShareUrl - The URL that will be shared on Facebook. Default: current page url.
 
-_Twitter variables_
-* twitterShareUrl - The URL that will be shared on Twitter. Default: current page url.
-* twitterShareMessage - The actual tweet message. Default: current page title.
-* twitterUsername - Will add via @twitterUsename to the tweet if set to anything but "". Default "".
+##Installation and how to use
+Download the script and include it in your html as such:
+``` html
+<script type="text/javascript" src="path/to/rsb.js"></script>
+```
 
-_Pinterest variables_
-* pinterestShareUrl - The URL that will be shared on Pinterest. Default: current page url.
-* pinterestDescription = The description of the pin. Default: current page title.
+You probably also want to include the default css:
+``` html
+<link rel="stylesheet" href="path/to/rsb.css" />
+```
 
-_Google+ variables_
-* googleplusUrl - The URL that will be shared on Google+. Default: current page url.
+Lastly add this: 
+``` html
+<script type="text/javascript">
+  rsb();
+</script>
+```
+Rsb automatically look for one or several elements with the class rsb to hook on to. Of course you can change this, see settings below.
 
-_E-mail variables_
-* emailSubject = The subject of the e-mail. Default: current page title.
-* emailMessage = The content of the e-mail. Default: current page url.
-* emailTo - Optional default e-mail to send to. Default: "".
+
+##Default Settings
+###Global
+* selector _(string)_ - The selector that rsb will look for. __Default:__ '.rsb'.
+* position _(string)_ - Position of the buttons. Available values: 'top', 'bottom'. Invalid values will return 'top'. __Default/Wildcard:__ 'top'.
+* zIndex _(number)_ -  Starting z-index, added so that rsb will be easily compatible with z-index heavy sites. __Default:__ 10.
+
+###Fcaebook
+* facebookShareUrl _(string)_ - The URL that will be shared on Facebook. __Default:__ Current page url (_document.location_).
+
+###Twitter variables
+* twitterShareUrl _(string)_ - The URL that will be shared on Twitter. __Default:__ Current page url (_document.location_).
+* twitterShareMessage _(string)_ - The actual tweet message. Default: current page title (_document.tile_).
+* twitterUsername _(string)_ - Will add _"via @twitterUsename"_ to the tweet if set to anything but ''. __Default:__ ''.
+
+###Pinterest variables
+* pinterestShareUrl _(string)_ - The URL that will be shared on Pinterest. __Default:__ Current page url (_document.location_).
+* pinterestDescription _(string)_ - The description of the pin. __Default:__ Current page title (_document.title_).
+
+###Google+ variables
+* googleplusUrl _(string)_ - The URL that will be shared on Google+. __Default:__ Current page url (_document.location_).
+
+###E-mail variables
+* emailSubject _(string)_ - The subject of the e-mail. __Default:__ current page title (_document.title_).
+* emailMessage _(string)_ - The content of the e-mail. __Default:__ current page url (document.location).
+* emailTo _(string)_ - Optional default e-mail to send to. __Default:__ ''.
+
 
 ##ToDo
-* Move settings to a more convenient place
-* Give the option of multiple share buttons on a single page with their own settings
+* ~~Move settings to a more convenient place~~
+* ~~Give the option of multiple share buttons on a single page with their own settings~~
 * Add support for left and right button positions.
-* Add settings for button size and placement for those who don't want to edit the css.
+* Add settings for button size and placement.
+* Backward compatibility in older browsers (javascript animations)?
--- rsb.js
+++ rsb.js
@@ -1,80 +1,79 @@
-/* Variables */
-var zIndex = 10;
-var triggerSize = 50;
-var buttonSize = 40;
+/* Global Variables */
 var globalUrl = encodeURI(document.location);
 var globalMessage = document.title;
 
-//Facebook variables
-var facebookShareUrl = globalUrl;
-
-//Twitter variables
-var twitterShareUrl = globalUrl;
-var twitterShareMessage = globalMessage;
-var twitterUsername = "";
-
-//Pinterest variables
-var pinterestShareUrl = globalUrl;
-var pinterestDescription = globalMessage;
-
-//Google+ variables
-var googleplusUrl = globalUrl;
-
-//E-mail variables
-var emailSubject = globalMessage;
-var emailMessage = globalUrl;
-var emailTo = "";
-
-
-
-var rsb = function(pos) {
+var defaultSettings = {
+  selector: '.rsb',
+  position: 'top',
+  zIndex: 10,
+  facebookShareUrl: globalUrl,
+  twitterShareUrl: globalUrl,
+  twitterShareUrl: globalUrl,
+  twitterShareMessage: globalMessage,
+  twitterUsername: '',
+  pinterestShareUrl: globalUrl,
+  pinterestDescription: globalMessage,
+  googleplusUrl: globalUrl,
+  emailSubject: globalMessage,
+  emailMessage: globalUrl,
+  emailTo: ''
+};
+  
+var rsb = function(settings) {
+  
+  console.log(settings);
+  
+  settings = $.extend(defaultSettings, settings);
+  
+  /* Add default class used in css */
+  $(settings.selector).addClass('rsb-wrapper');
   
   /* Append our html code */
-  $('.rsb').append('<div class="rsb-trigger"></div><div class="rsb-buttons"><a class="facebook">Facebook</a><a class="twitter">Twitter</a><a class="pinterest">Twitter</a><a class="googleplus">Email</a><a class="email">E-mail</a></div>');
+  $(settings.selector).append('<div class="rsb-wrapper"><div class="rsb-trigger"></div><div class="rsb-buttons"><a class="facebook">Facebook</a><a class="twitter">Twitter</a><a class="pinterest">Twitter</a><a class="googleplus">Email</a><a class="email">E-mail</a></div></div>');
   
   
   /* Set main class to determine where the buttons go, working values are top, and bottom. Default is top, any incorrect value will return top */
-  if(pos === "bottom") {
-    $('.rsb').addClass(pos);
+  if(settings.position === 'bottom') {
+    $(settings.selector).addClass(settings.position);
   }
   else {
-    $('.rsb').addClass("top");
+    $(settings.selector).addClass('top');
   }
   
   
   /* Add child classes instead of using :nth-child in css. This could probably look also look at the social media class instead. */
-  $('.rsb-buttons').each(function() {
+  $(settings.selector).find('.rsb-buttons').each(function() {
     $(this).find('a').each(function(i) {
-      $(this).addClass("rsb-"+(i+1));
+      $(this).addClass("rsb-item-"+(i+1));
     });
   });
   
   
   /* Set z-index from variable so we can play nicely with other z-index objects */
-  $('.rsb').css('z-index', zIndex);
-  $('.rsb .rsb-trigger').css('z-index', zIndex+15);
-  $('.rsb .rsb-buttons').css('z-index', zIndex+5);
-  $('.rsb .rsb-buttons a').css('z-index', zIndex+10);
+  $(settings.selector).css('z-index', settings.zIndex);
+  $(settings.selector).find('.rsb-trigger').css('z-index', settings.zIndex+15);
+  $(settings.selector).find('.rsb-buttons').css('z-index', settings.zIndex+5);
+  $(settings.selector).find('.rsb-buttons a').css('z-index', settings.zIndex+10);
   
   
   /* Link customization */
   /* Twitter */
-  if(twitterUsername != "") {
-    twitterUsername = "&via=" + twitterUsername;
+  if(settings.twitterUsername != "") {
+    settings.twitterUsername = "&via=" + settings.twitterUsername;
   }
   
   
   /* Links */
-  $('.rsb-buttons a:not(.email)').attr("onclick", "javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=335,width=600');return false;");
+  $(settings.selector).find('.rsb-buttons a:not(.email)').attr("onclick", "javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=335,width=600');return false;");
   
-  $('.rsb-buttons .facebook').attr('href', "https://www.facebook.com/sharer/sharer.php?u=" + facebookShareUrl);
-  $('.rsb-buttons .twitter').attr('href', "http://twitter.com/share?url=" + twitterShareUrl + twitterUsername + "&text=" + twitterShareMessage);
-  $('.rsb-buttons .pinterest').attr('href', "http://pinterest.com/pin/create/link/?url=" + pinterestShareUrl + "&description=" + pinterestDescription);
-  $('.rsb-buttons .googleplus').attr({
-    href: "https://plus.google.com/share?url=" + googleplusUrl,
+  $(settings.selector).find('.rsb-buttons .facebook').attr('href', "https://www.facebook.com/sharer/sharer.php?u=" + settings.facebookShareUrl);
+  $(settings.selector).find('.rsb-buttons .twitter').attr('href', "http://twitter.com/share?url=" + settings.twitterShareUrl + settings.twitterUsername + "&text=" + settings.twitterShareMessage);
+  $(settings.selector).find('.rsb-buttons .pinterest').attr('href', "http://pinterest.com/pin/create/link/?url=" + settings.pinterestShareUrl + "&description=" + settings.pinterestDescription);
+  $(settings.selector).find('.rsb-buttons .googleplus').attr({
+    href: "https://plus.google.com/share?url=" + settings.googleplusUrl,
     onclick: "javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"
   });
-  $('.rsb-buttons .email').attr('href', "mailto:" + emailTo + "?Subject=" + emailSubject + "&Body=" + emailMessage);
+  $(settings.selector).find('.rsb-buttons .email').attr('href', "mailto:" + settings.emailTo + "?Subject=" + settings.emailSubject + "&Body=" + settings.emailMessage);
   
   
   /* Button size for future dynamicness , not in use wright now*/
@@ -83,15 +82,10 @@ var rsb = function(pos) {
   
   
   /* Do stuff! */
-  $('.rsb-trigger').each(function() {
+  $(settings.selector).find('.rsb-trigger').each(function() {
     $(this).click(function() {
       $(this).parent().toggleClass('open');
     });
   });
   
-  
 };
-
-twitterUsername = "olofbrickarp";
-
-
